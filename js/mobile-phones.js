0d200f932d523d7e3cdd604a97ec44040912bb3c
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2026-04-16 20:05:19 +0200

    opg1-3

---
+++ js/mobile-phones.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const mobilePhones = [
+  {
+    id: 1,
+    brandname: "Apple",
+    brandmodel: "iPhone 15",
+    color: "black",
+    price: "3999",
+    memory: "128 GB",
+    img: "img/iphone-15-128-black.webp",
+  },
+  {
+    id: 2,
+    brandname: "Apple",
+    brandmodel: "iPhone 16",
+    color: "black",
+    price: "4999",
+    memory: "128 GB",
+    img: "img/iphone-16-128-black.webp",
+  },
+  {
+    id: 3,
+    brandname: "Apple",
+    brandmodel: "iPhone 17",
+    color: "black",
+    price: "8999",
+    memory: "256 GB",
+    img: "img/iphone-17-256-black.webp",
+  },
+  {
+    id: 4,
+    brandname: "Apple",
+    brandmodel: "iPhone 17",
+    color: "white",
+    price: "8999",
+    memory: "256 GB",
+    img: "img/iphone-17-256-white.webp",
+  },
+  {
+    id: 5,
+    brandname: "Apple",
+    brandmodel: "iPhone 17 Pro Max",
+    color: "black",
+    price: "10999",
+    memory: "256 GB",
+    img: "img/iphone-17-pro-max-256-black.webp",
+  },
+];
+//Opgave 3 – Opret datastrukturen mobilePhones med de første 5 telefoner
